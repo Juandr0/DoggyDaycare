4c229c0fef7cbb0385692a3c55cc4ca0196f7b8a
Alex <alexander.cg.carlsson@gmail.com>
2023-03-31 16:45:48 +0200

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,33 @@
+import './navbar.css';
+import '../App'
 
 
+const Navbar = (props) => {
+    return (
+        <nav class='nav'>
+            <a href="/#" className='nav_brandname'>Doggy Daycare</a>
+            <ul className='nav_menu'>
+                <li className="nav_items">
+                    <a href="/#">Home</a>
+                </li>
 
+                <li className="nav_items">
+                    <a href="/#"> Our Dogs</a>
+                </li>
 
-const Navbar = () => {
-    return(
-        <nav class = 'nav'>
-           <a href="/#">Hem</a>
-           <a href="/#">Hundar</a>
-           <a href="/#">Om oss</a>
+                <li className="nav_items">
+                    <a href="/#">About us</a>
+                </li>
+            </ul>
 
+            <div className="nav_toggler">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
 
         </nav>
-    )
+    );
 
 
 }
