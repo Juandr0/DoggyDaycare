199c09298eb7dbe668b28adc7d20b7294fefef0c
Alex <alexander.cg.carlsson@gmail.com>
2023-04-03 22:24:57 +0200

    added design for wrapper and dog image

--- src/ComponentScreens/Catalogue.jsx
+++ src/ComponentScreens/Catalogue.jsx
@@ -1,41 +1,56 @@
 import React, { useState, useEffect } from 'react';
-
+import './catalogue.css'
 const apiUrl = 'https://api.jsonbin.io/v3/b/6422b9c8c0e7653a0597d126'
 
 const Catalogue = (props) => {
-  const [dogs, setDogs] = useState([]);
+    const [dogs, setDogs] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      console.log('data' + data);
-      setDogs(data);
-      localStorage.setItem('dogs', JSON.stringify(data.record));
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            setDogs(data);
+            localStorage.setItem('dogs', JSON.stringify(data.record));
+        };
 
-    const cachedDogs = localStorage.getItem('dogs');
-    if (cachedDogs) {
-      console.log('Dogs are cached!')
-      setDogs(JSON.parse(cachedDogs));
-    } else {
-      console.log('No doggos cached, Fetching!');
-      fetchData();
-    }
+        //lägg till LocalStoarge check ifall datan redan finns, isf fetcha inte ny.
+        const cachedDogs = localStorage.getItem('dogs');
+        if (cachedDogs) {
+            console.log('Dogs are cached!')
+            setDogs(JSON.parse(cachedDogs));
+        } else {
+            console.log('No doggos cached, Fetching!');
+            fetchData();
+        }
+    }, []);
 
-  }, []);
-
-  return (
-    <div>
-      <h2>Our stars!</h2>
-      {dogs.map((dog, index) => (
-        <div key={index}>
-          <h3>{dog.name}</h3>
-          <img src={dog.img} alt={dog.name} />
+    return (
+        <div className='dogCard_container'>
+            <h2>Our stars!</h2>
+            <div className='dogImage_container'>
+                {dogs.map((dog, index) => (
+                    <div key={index} className='dogImage_wrapper dogCard' onClick={() => {cardClickHandler(dog)}}>
+                        <h3>{dog.name} the {dog.breed}</h3>
+                            <img className='dogImage dogCard' src={dog.img} alt={dog.name} />
+                            <p>Is currently at the daycare? {isDogPresent(dog)}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      ))}
-    </div>
-  );
+    );
 };
+
+const cardClickHandler = (props) => {
+    console.log('klick ' + props.name)
+} 
+
+const isDogPresent = (prop) => {
+    if (prop.present) {
+        return "Yes" 
+    } else {
+        return "No"
+    }
+}
+
 
 export default Catalogue;
